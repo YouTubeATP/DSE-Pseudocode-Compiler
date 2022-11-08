5cb5a0f9f244ae76a19c1ec0a61bef0034ac0cb0
YouTubeATP <46191535+YouTubeATP@users.noreply.github.com>
2022-11-09 01:34:11 +0800

    Change example

--- examples/example.py
+++ examples/example.py
@@ -1,11 +1,11 @@
 # Convert *.pc.txt to *.py
-
 from compiler import compile
 import os
 
-for file in os.listdir(os.path.relpath("./examples")):
+for file in os.listdir(os.path.abspath("./examples")):
+    print(file)
     if file.endswith(".pc.txt"):
-        with open(os.path.relpath(f"./examples/{file}"), "r") as f:
+        with open(os.path.abspath(f"./examples/{file}"), "r") as f:
             compiled_code = compile(f.read())
-        with open(os.path.relpath(f"./examples/{file[:-7]}.py"), "w") as f:
+        with open(os.path.abspath(f"./examples/{file[:-7]}.py"), "w") as f:
             f.write(compiled_code)
