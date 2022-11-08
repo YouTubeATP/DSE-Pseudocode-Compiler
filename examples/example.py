2be024470429c1464660ea750ad866581a657738
YouTubeATP <46191535+YouTubeATP@users.noreply.github.com>
2022-11-09 00:28:31 +0800

    Add example

---
+++ examples/example.py
@@ -0,0 +1,12 @@
+# Convert *.pc.txt to *.py
+
+from compiler import compile
+import os
+
+for file in os.listdir(os.path.relpath("./examples")):
+    print(file)
+    if file.endswith(".pc.txt"):
+        with open(os.path.relpath(f"./examples/{file}"), "r") as f:
+            compiled_code = compile(f.read())
+        with open(os.path.relpath(f"./examples/{file[:-7]}.py"), "w") as f:
+            f.write(compiled_code)
